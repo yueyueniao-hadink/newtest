395a6f3ab3913b2139c586a2f65528e1f40aa248
yueyueniao_hadink <let_s_go_hadink@126.com>
2021-07-29 00:04:51 +0800

    t2

--- src/main/java/com/atguigu/Test01.java
+++ src/main/java/com/atguigu/Test01.java
@@ -5,5 +5,6 @@ import java.util.ArrayList;
 public class Test01 {
     public static void main(String[] args) {
         ArrayList arrayList = new ArrayList();
+        System.out.println("test");
     }
 }
